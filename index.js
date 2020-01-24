794a305c33285d4fcee244a621d3e45b80c0765d
diegoaraujo85 <doasalah@gmail.com>
2020-01-24 18:50:18 -0300

    route params

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const server = express();
 //route params = /users/1
 //request body = { "name": "Diego" }
 
-server.get("/teste", (req, res) => {
-  // localhost:3000/teste?nome=Diego
-  const nome = req.query.nome;
+server.get("/users/:id", (req, res) => {
+  // localhost:3000/users/1
+  const id = req.params.id;
 
-  return res.json({ message: `Hello ${nome}` });
+  return res.json({ message: `Buscando o usuário ${id}` });
 });
 
 server.listen(3000);
