a924f52aa838e3876f92472a64b7bada0df9b620
diegoaraujo85 <doasalah@gmail.com>
2020-01-24 23:57:15 -0300

    nodemon + CRUD

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
 
 //query params = ?teste=1
 //route params = /users/1
 //request body = { "name": "Diego" }
 
-const users = ["Diego", "Icaro", "Cairo"]
+//CRUD - Create, Read, Update, Delete
 
+const users = ["Diego", "Icaro", "Cairo", "Lis"];
+
+//lista todos users
+server.get("/users", (req, res) => {
+  return res.json(users);
+});
+
+//lista 1 user
 server.get("/users/:index", (req, res) => {
-  // localhost:3000/users/1
-  //antes
-  //const id = req.params.id;
-  //depois da desestruturação
+  const { index } = req.params;
+  return res.json(users[index]);
+});
+
+//criar usuario
+server.post("/users", (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+  return res.json(users);
+});
+
+//editar 1 user
+server.put("/users/:index", (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+  return res.json(users);
+});
+
+//deletar 1 user
+server.delete("/users/:index", (req, res) => {
   const { index } = req.params;
 
-  return res.json(users[index]);
+  users.splice(index, 1);
+  return res.send();
 });
 
 server.listen(3000);
